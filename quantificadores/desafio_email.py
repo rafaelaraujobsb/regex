b7067fee7d56ee95a284cc3c2e589b966f77ad38
Rafael Araujo <bsb.rafaelaraujo@gmail.com>
2018-12-07 15:46:59 -0200

    correção regex email

--- quantificadores/desafio_email.py
+++ quantificadores/desafio_email.py
@@ -2,11 +2,13 @@ import re
 
 lista = """
     Lista emails:
-        - xyz@xc.bg
-        - tarf@gmail.com
-        - apple@apple.com
+    - fulano@cod3r.com.br
+    - xico@gmail.com
+    - joao@empresa.info.br
+    - maria_silva@registro.br
+    - rafa.sampaio@yahoo.com
 """
 
-regex = re.compile(r"\w+@{1}\w{2,9}\.com.?")
+regex = re.compile(r"[A-z0-9]+@[A-z0-9]+\.[A-z]{2,4}")
 
 print(re.findall(regex, lista))
